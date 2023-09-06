266a72ef684f9d1af07b7432d223621581ba4301
Darren Clark <darclar@ut.utm.edu>
2023-09-06 12:17:28 -0500

    Fourth commit: Added the sum function.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+int sum(int number);
+
+
+
 int main(){
   cout << "Hello World!" << endl;
+  cout << "Please enter a number." << endl;
+  int number = 0;
+  cin >> number;
+  cout <<  sum(number);
+
 
   return 0;
+}
+
+int sum(int number){
+  int x = number;
+  for (int i = 0; i < number; i++) {
+    x += i;
+  }
+
+  return x;
+
 }
